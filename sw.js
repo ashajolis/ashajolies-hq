66bbc15efdaa23ec262edf3235b9299977a41ee2
Rootsday <ashajolis@gmail.com>
2026-04-25 10:06:10 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'ajhq-v2';
+
+self.addEventListener('install', e => e.waitUntil(self.skipWaiting()));
+
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+));
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const r = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, r));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
